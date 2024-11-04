75998d69d85c7f2726850a6e5c5bfcacab1f5a33
Darwin Grisales <darwingrisalesg@gmail.com>
2024-11-04 12:44:58 -0500

    fix: change colorscheme

--- init.lua
+++ init.lua
@@ -278,12 +278,12 @@ require('lazy').setup({
   --     vim.cmd.colorscheme 'catppuccin-macchiato'
   --   end,
   -- },
-  {
-    'nyoom-engineering/oxocarbon.nvim',
-    config = function()
-      vim.cmd.colorscheme 'oxocarbon'
-    end,
-  },
+  -- {
+  --   'nyoom-engineering/oxocarbon.nvim',
+  --   config = function()
+  --     vim.cmd.colorscheme 'oxocarbon'
+  --   end,
+  -- },
   -- {
   --   'craftzdog/solarized-osaka.nvim',
   --   lazy = true,
@@ -622,6 +622,7 @@ require('lazy').setup({
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
         -- tsserver = {},
+	ts_ls = {},
         --
 
         lua_ls = {
@@ -703,7 +704,7 @@ require('lazy').setup({
         -- python = { "isort", "black" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        javascript = { "prettierd", "prettier", stop_after_first = true },
       },
     },
   },
@@ -824,64 +825,64 @@ require('lazy').setup({
     end,
   },
 
-  -- { -- You can easily change to a different colorscheme.
-  --   -- Change the name of the colorscheme plugin below, and then
-  --   -- change the command in the config to whatever the name of that colorscheme is.
-  --   --
-  --   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-  --   'folke/tokyonight.nvim',
-  --   priority = 1000, -- Make sure to load this before all the other start plugins.
-  --   config = function()
-  --     local transparent = true -- set to true if you would like to enable transparency
-  --
-  --     local bg = '#011628'
-  --     local bg_dark = '#011423'
-  --     local bg_highlight = '#143652'
-  --     local bg_search = '#0A64AC'
-  --     local bg_visual = '#275378'
-  --     local fg = '#CBE0F0'
-  --     local fg_dark = '#B4D0E9'
-  --     local fg_gutter = '#627E97'
-  --     local border = '#547998'
-  --
-  --     require('tokyonight').setup {
-  --       style = 'night',
-  --       transparent = transparent,
-  --       styles = {
-  --         sidebars = transparent and 'transparent' or 'dark',
-  --         floats = transparent and 'transparent' or 'dark',
-  --       },
-  --       on_colors = function(colors)
-  --         colors.bg = bg
-  --         colors.bg_dark = transparent and colors.none or bg_dark
-  --         colors.bg_float = transparent and colors.none or bg_dark
-  --         colors.bg_highlight = bg_highlight
-  --         colors.bg_popup = bg_dark
-  --         colors.bg_search = bg_search
-  --         colors.bg_sidebar = transparent and colors.none or bg_dark
-  --         colors.bg_statusline = transparent and colors.none or bg_dark
-  --         colors.bg_visual = bg_visual
-  --         colors.border = border
-  --         colors.fg = fg
-  --         colors.fg_dark = fg_dark
-  --         colors.fg_float = fg
-  --         colors.fg_gutter = fg_gutter
-  --         colors.fg_sidebar = fg_dark
-  --       end,
-  --     }
-  --
-  --     vim.cmd 'colorscheme tokyonight'
-  --   end,
-  --   init = function()
-  --     -- Load the colorscheme here.
-  --     -- Like many other themes, this one has different styles, and you could load
-  --     -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-  --     vim.cmd.colorscheme 'tokyonight-night'
-  --
-  --     -- You can configure highlights by doing something like:
-  --     vim.cmd.hi 'Comment gui=none'
-  --   end,
-  -- },
+  { -- You can easily change to a different colorscheme.
+    -- Change the name of the colorscheme plugin below, and then
+    -- change the command in the config to whatever the name of that colorscheme is.
+    --
+    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+    'folke/tokyonight.nvim',
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    config = function()
+      local transparent = true -- set to true if you would like to enable transparency
+
+      local bg = '#011628'
+      local bg_dark = '#011423'
+      local bg_highlight = '#143652'
+      local bg_search = '#0A64AC'
+      local bg_visual = '#275378'
+      local fg = '#CBE0F0'
+      local fg_dark = '#B4D0E9'
+      local fg_gutter = '#627E97'
+      local border = '#547998'
+
+      require('tokyonight').setup {
+        style = 'night',
+        transparent = transparent,
+        styles = {
+          sidebars = transparent and 'transparent' or 'dark',
+          floats = transparent and 'transparent' or 'dark',
+        },
+        on_colors = function(colors)
+          colors.bg = bg
+          colors.bg_dark = transparent and colors.none or bg_dark
+          colors.bg_float = transparent and colors.none or bg_dark
+          colors.bg_highlight = bg_highlight
+          colors.bg_popup = bg_dark
+          colors.bg_search = bg_search
+          colors.bg_sidebar = transparent and colors.none or bg_dark
+          colors.bg_statusline = transparent and colors.none or bg_dark
+          colors.bg_visual = bg_visual
+          colors.border = border
+          colors.fg = fg
+          colors.fg_dark = fg_dark
+          colors.fg_float = fg
+          colors.fg_gutter = fg_gutter
+          colors.fg_sidebar = fg_dark
+        end,
+      }
+
+      vim.cmd 'colorscheme tokyonight'
+    end,
+    init = function()
+      -- Load the colorscheme here.
+      -- Like many other themes, this one has different styles, and you could load
+      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+      vim.cmd.colorscheme 'tokyonight-night'
+
+      -- You can configure highlights by doing something like:
+      vim.cmd.hi 'Comment gui=none'
+    end,
+  },
 
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
@@ -927,7 +928,7 @@ require('lazy').setup({
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'typescript', 'javascript' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
