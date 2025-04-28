fcb1e92b637d1f5644910d5b1cee80d9606f4116
Jhon Darwin Grisales Giraldo <darwingrisalesg@gmail.com>
2025-04-28 17:54:17 -0500

    feature: add tokyonight theme

--- init.lua
+++ init.lua
@@ -306,19 +306,19 @@ require('lazy').setup({
   --     vim.cmd.colorscheme 'gruvbox-material'
   --   end,
   -- },
-  {
-    'gmr458/vscode_modern_theme.nvim',
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require('vscode_modern').setup {
-        cursorline = true,
-        transparent_background = false,
-        nvim_tree_darker = true,
-      }
-      vim.cmd.colorscheme 'vscode_modern'
-    end,
-  },
+  -- {
+  --   'gmr458/vscode_modern_theme.nvim',
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     require('vscode_modern').setup {
+  --       cursorline = true,
+  --       transparent_background = false,
+  --       nvim_tree_darker = true,
+  --     }
+  --     vim.cmd.colorscheme 'vscode_modern'
+  --   end,
+  -- },
   -- {
   --   'catppuccin/nvim',
   --   name = 'catppuccin',
@@ -839,6 +839,8 @@ require('lazy').setup({
         typescript = { 'prettierd', 'prettier', stop_after_first = true },
         javascriptreact = { 'prettierd', 'prettier', stop_after_first = true },
         typescriptreact = { 'prettierd', 'prettier', stop_after_first = true },
+        html = { 'prettierd', 'prettier', stop_after_first = true },
+        css = { 'prettierd', 'prettier', stop_after_first = true },
       },
     },
   },
@@ -940,6 +942,27 @@ require('lazy').setup({
       -- Shows a signature help window while you type arguments for a function
       signature = { enabled = true },
     },
+  },
+  { -- You can easily change to a different colorscheme.
+    -- Change the name of the colorscheme plugin below, and then
+    -- change the command in the config to whatever the name of that colorscheme is.
+    --
+    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+    'folke/tokyonight.nvim',
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    config = function()
+      ---@diagnostic disable-next-line: missing-fields
+      require('tokyonight').setup {
+        styles = {
+          comments = { italic = false }, -- Disable italics in comments
+        },
+      }
+
+      -- Load the colorscheme here.
+      -- Like many other themes, this one has different styles, and you could load
+      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+      vim.cmd.colorscheme 'tokyonight-night'
+    end,
   },
 
   -- { -- You can easily change to a different colorscheme.
