4c1395346a90027c56b9446749c199e7d61301c5
Jhon Darwin Grisales Giraldo <darwingrisalesg@gmail.com>
2025-09-01 20:03:20 -0500

    chore: change theme to gruvbox original

--- lua/custom/plugins/active_colorscheme.lua
+++ lua/custom/plugins/active_colorscheme.lua
@@ -4,5 +4,6 @@ local one_dark = require 'custom.plugins.themes.one_dark'
 local tokyonight_custom = require 'custom.plugins.themes.tokyonight_custom'
 local dark_modern = require 'custom.plugins.themes.dark_modern'
 local rose_pine = require 'custom.plugins.themes.rose-pine'
+local gruvbox = require 'custom.plugins.themes.gruvbox'
 
-return rose_pine
+return gruvbox
